f9f95d42d03aea468be81f8cb6efb7e0a1d54b86
AlonePainPeanut <139093156+noturcmd@users.noreply.github.com>
2024-10-13 14:29:25 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/datamahasiswa', function () {
+    return view('datamahasiswa');
+});
+
+
