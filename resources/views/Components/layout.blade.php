bcda7f69c59e1649035764b8b445654070adcf6d
Septian Dwi Cahyo Nugroho <septiandwi@student.uns.ac.id>
2024-10-13 16:20:20 +0700

    update lagi

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -102,6 +102,13 @@
                 <div class="flex justify-center mx-auto mt-5">
                     <p class="text-2xl text-gray-600">{{ $slot }}</p>
                 </div>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <a href="#"
+                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
+                            started</a>
+                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
+                                aria-hidden="true">→</span></a>
+                    </div>
 
 
             </div>
