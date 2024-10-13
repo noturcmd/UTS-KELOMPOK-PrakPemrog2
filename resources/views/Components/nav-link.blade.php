4571537aaccc81e56f647d3772fe5c581590a675
AlonePainPeanut <139093156+noturcmd@users.noreply.github.com>
2024-10-13 15:34:35 +0700

    update nav-link.blade.php

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,7 +1,18 @@
 <script src="https://cdn.tailwindcss.com"></script>
 
-@props(['aktif' => false])
-<a 
-    class="text-sm font-semibold leading-6 text-gray-900 {{$aktif ? 'md:text-blue-700' : ''}}"
-    aria-current="{{$aktif ? 'page' : 'false'}}" 
-    {{$attributes }}>{{ $slot }}</a>
+@props(['aktif' => false, 'type' => 'a'])
+
+@if ($type === 'a')
+<a
+    class="{{ $aktif ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $aktif ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}
+</a>
+
+@else
+<button
+    class="{{ $aktif ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $aktif ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}
+</button>
+
+@endif
+
