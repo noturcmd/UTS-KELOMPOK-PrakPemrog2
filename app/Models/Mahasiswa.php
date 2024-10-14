ba3cf080507f14665ff2021d3bbf0ce5d9b78c16
AlonePainPeanut <139093156+noturcmd@users.noreply.github.com>
2024-10-14 11:00:41 +0700

    buat db dan model Mahasiswa

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+
+    use HasFactory;
+    protected $table = 'mahasiswas';
+    protected $fillable = ['nama', 'asal_kota', 'kode_asal_kota'];
+
+}
---
+++ database/factories/MahasiswaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Mahasiswa>
+ */
+class MahasiswaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama' => $this->faker->name(),
+            'asal_kota' => $this->faker->city(),
+            'kode_asal_kota' => strtoupper($this->faker->lexify('??')),
+        ];
+    }
+}
+
+
+
